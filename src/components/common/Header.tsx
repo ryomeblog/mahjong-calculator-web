320658eba8f3cf8fb809465b3acf1f9692177ca6
ryome <ryome.blog@gmail.com>
2026-02-11 21:48:46 +0900

    feat: ヘッダーにアプリ名の補足説明を追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,7 +6,10 @@ export function Header() {
   return (
     <header className="bg-slate-800">
       <div className="flex h-16 items-center justify-center">
-        <h1 className="text-xl font-bold text-slate-50">まじゃっぴー</h1>
+        <h1 className="text-xl font-bold text-slate-50">
+          まじゃっぴー
+          <span className="hidden lg:inline">（麻雀点数計算アプリ）</span>
+        </h1>
       </div>
     </header>
   )
